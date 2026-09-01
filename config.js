f02a184710d8ab269194cf87e6e4b8bf86203688
venkatvamshi <venkatreddy.d94@gmail.com>
2026-09-01 17:25:25 +0530

    Change SHEET_TAB_NAME from 'Menu' to 'menu'

--- config.js
+++ config.js
@@ -3,7 +3,7 @@ const RESTAURANT_CONFIG = {
   GOOGLE_SPREADSHEET_ID: "17cObi5AFpwQspEYFpRl5y2loidPoztLRx21aOQEui3I",
   
   // Name of the tab at the bottom of the sheet (case-sensitive)
-  SHEET_TAB_NAME: "Menu",
+  SHEET_TAB_NAME: "menu",
   
   GOOGLE_PLACE_ID: "ChIJIznANwAP6joRsC8ckf6OSuc",
   RESTAURANT_NAME: "Annapurna Tiffins",
